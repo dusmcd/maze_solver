fba8ae1284b03e039f66c47aca2249fd8d076ede
Dustin McDowell <dustinmcdowell1989@gmail.com>
2024-05-08 09:44:44 -0400

    drawing cells in window

---
+++ src/cell.py
@@ -0,0 +1,32 @@
+from line import Line, Point
+
+class Cell:
+    def __init__(self, x1, y1, x2, y2, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+        self.__win = win
+    
+    def draw(self, fill_color):
+        top_left = (self.__x1, self.__y1)
+        bottom_right = (self.__x2, self.__y2)
+        top_right = (self.__x2, self.__y1)
+        bottom_left = (self.__x1, self.__y2)
+
+        if self.has_left_wall:
+            line = Line(Point(top_left[0], top_left[1]), Point(bottom_left[0], bottom_left[1]))
+            self.__win.draw_line(line, fill_color)
+        if self.has_top_wall:
+            line = Line(Point(top_left[0], top_left[1]), Point(top_right[0], top_right[1]))
+            self.__win.draw_line(line, fill_color)
+        if self.has_right_wall:
+            line = Line(Point(top_right[0], top_right[1]), Point(bottom_right[0], bottom_right[1]))
+            self.__win.draw_line(line, fill_color)
+        if self.has_bottom_wall:
+            line = Line(Point(bottom_right[0], bottom_right[1]), Point(bottom_left[0], bottom_left[1]))
+            self.__win.draw_line(line, fill_color)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 from window import Window
 from line import Line, Point
+from cell import Cell
 
 def main():
     win = Window(1000, 1000)
-    line = Line(Point(100, 100), Point(200, 200))
-    line2 = Line(Point(80, 600), Point(95, 12))
-    win.draw_line(line, "red")
-    win.draw_line(line2, "black")
+    cell = Cell(100, 100, 200, 200, win)
+    cell.draw("red")
     win.wait_for_close()
 
 
